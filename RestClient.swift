d948c82ab208b9a45d3aa3ddfbcd2d2da542e1ec
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2018-04-25 14:43:32 -0700

    Update RestClient.swift

--- RestClient.swift
+++ RestClient.swift
@@ -1,9 +1,9 @@
 //
 //  RestClient.swift
-//  Temperature Converter
+//  MeliPayamak
 //
 //  Created by Amirhossein Mehrvarzi on 4/25/18.
-//  Copyright © 2018 Web In Dream. All rights reserved.
+//  Copyright © 2018 MeliPayamak. All rights reserved.
 //
 
 import Foundation
@@ -29,47 +29,21 @@ class RestClient
     
     func Send(to: String, from: String, message: String, isflash: Bool)
 {
-//    let values:[String: String] = [
-//        "username": self.UserName,
-//        "password": self.Password,
-//        "to": to,
-//        "from": from,
-//        "text": message,
-//        "isFlash": isflash.description
-//    ];
+
     let values = "username=\(self.UserName)&password=\(self.Password)&to=\(to)&from=\(from)&text=\(message)&isFlash=\(isflash.description)"
     
     let url = URL(string: endpoint)
     
     let theRequest = NSMutableURLRequest(url: url!)
     
-    //let msgLength = soapMessage.characters.count
-    
     theRequest.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-    //theRequest.addValue(String(msgLength), forHTTPHeaderField: "Content-Length")
-    theRequest.httpMethod = "POST"
-    theRequest.httpBody = //NSKeyedArchiver.archivedData(withRootObject: values)
-        values.data(using: String.Encoding.utf8, allowLossyConversion: false) // or false
     
-
+    theRequest.httpMethod = "POST"
+    theRequest.httpBody = values.data(using: String.Encoding.utf8, allowLossyConversion: false)
     let connection = NSURLConnection(request: theRequest as URLRequest, delegate: self, startImmediately: true)
     
     connection!.start()
 
-    
-    
-    
-    
-    
-//    var content = new FormUrlEncodedContent(values);
-//    
-//    using (var httpClient = new HttpClient())
-//    {
-//    var response = httpClient.PostAsync(endpoint + sendOp, content).Result;
-//    var responseString = response.Content.ReadAsStringAsync().Result;
-//    
-//    return JsonConvert.DeserializeObject<RestResponse>(responseString);
-//    }
     }
     
     
@@ -87,21 +61,13 @@ class RestResponse : NSObject
     init(mutableData: NSData) {
         super.init()
         
-//        let JSONData = mutableData.data(using: String.Encoding.utf8, allowLossyConversion: false)
-        
         let JSONDictionary = try? JSONSerialization.jsonObject(with: mutableData as Data, options: []) as! Dictionary<String, AnyObject>
-        
         
         // Loop
         for (key, value) in JSONDictionary! {
             let keyName = key as String
             
             let keyValue: String = String(describing: value)
-//            var keyValue: String = ""
-//            if(value is NSNumber){
-//                keyValue = value as! String
-//            }
-            
             
             // If property exists
             if (self.responds(to: NSSelectorFromString(keyName))) {
@@ -115,5 +81,3 @@ class RestResponse : NSObject
     
     
 }
-
-
